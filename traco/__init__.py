e45d9d84dee276896dfda30b78a13457cdce9458
Petr Kolenko <47007786+kolenpe1@users.noreply.github.com>
2023-05-14 23:11:29 +0200

    Update __init__.py
    from stackexchange?

--- traco/__init__.py
+++ traco/__init__.py
@@ -1,3 +1,6 @@
 from .source import vec_fact, vec_diff, factor, shelx_hkl, read_HKL_file, read_SCA_file, mtz_I, mtz_F, check_output
 
 __name__ == ['vec_fact', 'vec_diff', 'factor', 'shelx_hkl', 'read_HKL_file', 'read_SCA_file', 'mtz_I', 'mtz_F', 'check_output']
+
+# from .source import *
+#__all__ == [...]
